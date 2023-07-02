2c5287934844bd601373ac37514334485ca8f0ed
Hasibul Miraz <hasibul.miraz@gmail.com>
2023-07-03 02:43:56 +0600

    formating code

--- data/filter_data.py
+++ data/filter_data.py
@@ -2,14 +2,60 @@ import json
 import pandas
 
 class FilterData:
-    data_path = 'main data/data.json'
-    frequent_used_words_path = 'main data/unigram_freq.csv'
-    data = "helo"
-    
-    def get_data(self):
-        print(self.data)
+    data_path = './source/data.json'
+    frequent_used_words_path = './source/unigram_freq.csv'
+    save_data_path = './source/final_data.json'
 
+    data = dict()
+    
+
+    def read_files(self, path):
+        if "json" in path:
+            with open(path, "r") as read_file:
+                return json.load(read_file)
+        elif "csv" in path:
+            df = pandas.read_csv(path)
+            data_csv = df.drop("count", axis=1)
+            return data_csv.values.tolist()
+        
+    def save_json_file(self, path):
+        with open(path, 'w') as write_file:
+            json.dump(self.data, write_file, indent=4)
+    
+    def get_data_from_file(self):
+        words_with_url = self.read_files(self.data_path)
+        frequent_words = self.read_files(self.frequent_used_words_path)
+
+        return words_with_url, frequent_words
+    
+    def get_relevant_data(self):
+        data1, data2 = self.get_data_from_file()
+
+        to_go = len(data1)
+        
+        for word, url in data1.items():
+            print(f"{to_go} left")
+            to_go-=1
+            if [word] in data2:
+                self.data[word] = url
+    
+    def alphabatical_threading_data(self):
+        data = self.data
+        alphabet = set()
+        new_data = dict()
+        for word, url in data.items():
+            if word[0] not in alphabet:
+                alphabet.add(word[0])
+                new_data[word[0]] = dict()
+            new_data[word[0]][word] = url
+        self.data = new_data
+    
+    def process(self):
+        self.get_relevant_data()
+        self.alphabatical_threading_data()
+        self.save_json_file(self.save_data_path)
+        
 
 
 f = FilterData()
-f.get_data()
+f.process()
