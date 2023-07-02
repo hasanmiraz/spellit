6e4ee1a181dc0ff92f26b6b63a57a8bcfe4f37f4
Hasibul Miraz <hasibul.miraz@gmail.com>
2023-07-02 22:59:07 +0600

    data fetching complete

--- data/collect_mp3.py
+++ data/collect_mp3.py
@@ -14,9 +14,9 @@ def create_folders():
             os.mkdir(folder)
 
     with open('./log/failed.json', "w") as failed_file:
-        pass
+        json.dump(failed_dict, failed_file, indent=4)
     with open('./log/last.json', "w") as last_data_file:
-        pass
+        json.dump(last_data, last_data_file, indent=4)
     print("folder created")
 
 def get_log_data():
@@ -27,8 +27,7 @@ def get_log_data():
         failed_dict = json.load(failed_file)
     with open('./log/last.json', "r") as last_data_file:
         last_data = json.load(last_data_file)
-    print(failed_dict)
-    print(last_data)
+
 
 def get_data():
     with open("final_data.json", "r") as data_f:
@@ -43,15 +42,56 @@ def save_data(word, url, type):
         failed_dict[word]=url
     elif type == "last":
         last_data = word
+
+def write_log():
+    with open("./log/failed.json", "w") as failed_file:
+        json.dump(failed_dict, failed_file, indent=4)
         
-def get_mp3_file(word, url):    
-    x = requests.get(url)
+def get_mp3_file(word, url):   
+    headers = {
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+            } 
+    x = requests.get(url, headers=headers)
     if x.status_code!=200:
-        save_data(word, "failed")
+        save_data(word, url, "failed")
+        return None
+    return x.content
+
+def write_mp3(word, mp3):
+    print(f"saving {word}")
+    filepath = f"./download/{word}.mp3"
+    with open(filepath, 'wb') as file:
+        file.write(mp3)
 
 def process():
     create_folders()
     get_log_data()
+    data, total_data = get_data()
 
+    break_at = 5
 
+    count_data = 0
+    for word, url in data.items():
+        count_data += 1
+        print(f"{count_data} out of {total_data} working on {word}")
+
+        if os.path.exists(f"./download/{word}.mp3"):
+            continue
+        
+        mp3_file = get_mp3_file(word, url)
+
+        if mp3_file == None:
+            print(f"failed at {count_data} word {word}")
+            save_data(word, url, "failed")
+            continue
+        
+        write_mp3(word, mp3_file)
+
+        # break_at -= 1
+        # if break_at == 0:
+        #     print("breaking")
+        #     break;
+
+        
 process()
+write_log()
--- data/test.py
+++ data/test.py
@@ -1,15 +1,19 @@
 import requests
 
-# x = requests.get("http://www.onelook.com/pronounce/macmillan/UK/abnormal-British-English-pronunciation.mp3")
-# print(type(x.status_code))
-# filename = "abnormal.mp3"
-# with open(filename, 'wb') as file:
-#     file.write(x.content)
+headers = {
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+}
 
-x = {"sdf":"ete"}
+x = requests.get(f"http://dictionary.cambridge.org/us/media/english/us_pron/u/usa/usad_/usad___006.mp3", headers=headers)
 print(x)
-def something():
-    global x
-    x["hekl"]="sdfjilj"
-    x["hkl"]="sd2444213j"
-print(x)
+filename = "abnormal.mp3"
+with open(filename, 'wb') as file:
+    file.write(x.content)
+
+# x = {"sdf":"ete"}
+# print(x)
+# def something():
+#     global x
+#     x["hekl"]="sdfjilj"
+#     x["hkl"]="sd2444213j"
+# print(x)
